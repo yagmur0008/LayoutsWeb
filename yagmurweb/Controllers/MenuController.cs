712bf6348b254a834411fbfdd35929b4d25400c8
Casper <Casper@DESKTOP-91JDMN9>
2026-03-02 21:06:21 +0300

    Add project files.

---
+++ yagmurweb/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace yagmurweb.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ yagmurweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace yagmurweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
